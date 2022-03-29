34c2d286e128e0e773694122b529accf2c8f2036
GaganC0834419 <c0834419@mylambton.ca>
2022-03-29 16:53:21 -0400

    Added Stripe payment gateway to do payments

--- app/src/main/java/com/project/betterbaskets/interfaces/Constants.java
+++ app/src/main/java/com/project/betterbaskets/interfaces/Constants.java
@@ -20,11 +20,14 @@ public interface Constants {
     public static final String REF_USERS = "users";
     public static final String REF_STORE_PRODUCTS = "store_products";
     public static final String REF_STORE_SALES= "store_sales";
+    public static final String REF_PAYMENTS= "payments";
     public static final String REF_STORES = "stores";
     public static final String REF_CUSTOMERS = "customers";
     public static final String PHONE_CODE = "+1";
     public static final String STORE_ID = "storeId";
     public static final String PROFILE_IMG = "profileImg";
+    public static final String RUPEE_SYMBOL = "$";
+    public static final String CLIENT_ID = "AWVBYefZIKZ_RLhFmV6CgwFKJPm_g3fZ14i1kw9B2Pt9UiRXo6AJjTBdt2ZeOoCjU2GxI8c0dvH5IXbm";
 
 
 
