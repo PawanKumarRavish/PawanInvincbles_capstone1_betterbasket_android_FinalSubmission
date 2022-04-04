261b3a5be6000423a35bf9818245b391db559d72
GaganC0834419 <c0834419@mylambton.ca>
2022-04-03 21:53:32 -0400

    Implemented Graphs for Store Sales

--- app/src/main/java/com/invicibles/betterbaskets/storeFragments/StoreHomeFrg.java
+++ app/src/main/java/com/invicibles/betterbaskets/storeFragments/StoreHomeFrg.java
@@ -2,7 +2,10 @@ package com.invicibles.betterbaskets.storeFragments;
 
 import static com.invicibles.betterbaskets.utilities.Utils.initialiseFirebase;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,6 +15,20 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.formatter.PercentFormatter;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+import com.github.mikephil.charting.utils.ColorTemplate;
+import com.github.mikephil.charting.utils.ViewPortHandler;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -26,8 +43,9 @@ import com.invicibles.betterbaskets.utilities.SharedPreference;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
-public class StoreHomeFrg extends BaseFrg {
+public class StoreHomeFrg extends BaseFrg /*implements OnChartValueSelectedListener*/ {
 
     StoreHomeFrgBinding binding;
     Users loggedStore;
@@ -35,6 +53,17 @@ public class StoreHomeFrg extends BaseFrg {
    int totalMoneyEarned=0;
    int totalSales=0;
     List<SaleModel> saleList;
+    List<PaymentModel> paymentList;
+
+    BarChart barChart;
+    BarData barData;
+    BarDataSet barDataSet;
+    ArrayList barEntries;
+
+
+
+    Legend legend;
+    PieDataSet dataSet;
 
     @Nullable
     @Override
@@ -50,12 +79,54 @@ public class StoreHomeFrg extends BaseFrg {
         databaseReference = initialiseFirebase();
         loggedStore= SharedPreference.getLoggedStore();
         saleList=new ArrayList<>();
+        paymentList=new ArrayList<>();
 
         binding.mStoreNameTv.setText("Hi, "+loggedStore.getName());
 
         getPaymentHistory();
 
         getStoreSales(loggedStore.getId());
+
+
+
+    }
+
+
+    private void initiateBarChart2(List<PaymentModel> paymentList) {
+        try {
+
+            binding.mBarChart.invalidate();
+            binding.mBarChart.clear();
+
+            getBarEntries2(paymentList);
+            barDataSet = new BarDataSet(barEntries, "Store Sales");
+            barData = new BarData(barDataSet);
+            binding.mBarChart.setData(barData);
+            barDataSet.setColors(ColorTemplate.JOYFUL_COLORS);
+            barDataSet.setValueTextColor(Color.BLACK);
+            barDataSet.setValueTextSize(18f);
+
+        }catch (Exception e){
+            Toast.makeText(getActivity(), e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+
+    private void getBarEntries2(List<PaymentModel> paymentList) {
+        barEntries = new ArrayList<>();
+        for(int i=0;i<paymentList.size();i++){
+
+            float amountToShow=0;
+            for(int j=0;j<paymentList.get(i).getSaleModel().getProductsList().size();j++){
+                amountToShow=amountToShow+Float.parseFloat(paymentList.get(i).getSaleModel().getProductsList().get(j).getSalePrice());
+            }
+
+            barEntries.add(new BarEntry(i, amountToShow));
+
+        }
+
+
     }
 
     private void getStoreSales(String loogedStoreId) {
@@ -79,6 +150,7 @@ public class StoreHomeFrg extends BaseFrg {
                 }
 
 
+
             }
 
             @Override
@@ -90,15 +162,79 @@ public class StoreHomeFrg extends BaseFrg {
 
     }
 
+    /*private void initiatePieChart(List<PaymentModel> paymentList) {
+        try {
+            ArrayList<Entry> yvalues = new ArrayList<Entry>();
+            ArrayList<String> xVals = new ArrayList<String>();
+
+            for (int i = 0; i < paymentList.size(); i++) {
+               // yvalues.add(new Entry(Float.valueOf(saleList.get(i).getAmount()), i));
+                float amountToShow=0;
+                for(int j=0;j<paymentList.get(i).getSaleModel().getProductsList().size();j++){
+                    amountToShow=amountToShow+Float.parseFloat(paymentList.get(i).getSaleModel().getProductsList().get(j).getSalePrice());
+                }
+                yvalues.add(new Entry(amountToShow, i));
+                xVals.add(paymentList.get(i).getStoreId());
+            }
+
+
+
+            dataSet = new PieDataSet(yvalues, "");
+            dataSet.setSliceSpace(3); // adding slice between pie chart
+            dataSet.setSelectionShift(5);
+
+
+            PieData data = new PieData(xVals, dataSet);
+                data.setValueFormatter(new PercentFormatter() {
+                    @Override
+                    public String getFormattedValue(float value, YAxis yAxis) {
+                        return mFormat.format(value);
+                    }
+
+                    @Override
+                    public String getFormattedValue(float value, Entry entry, int dataSetIndex, ViewPortHandler viewPortHandler) {
+                        return mFormat.format(value);
+                    }
+                });
+            binding.mBarChart.setData(data);
+            binding.mBarChart.setDescription("");
+
+            legend = binding.mBarChart.getLegend();
+            int colourCodes[]=legend.getColors();
+            legend.setForm(Legend.LegendForm.SQUARE);
+            legend.setPosition(Legend.LegendPosition.ABOVE_CHART_RIGHT);
+            legend.setXEntrySpace(7);
+            legend.setYEntrySpace(5);
+            legend.setWordWrapEnabled(true);
+
+            binding.mBarChart.setDrawHoleEnabled(true);
+            binding.mBarChart.setTransparentCircleRadius(25f);
+            binding.mBarChart.setHoleRadius(65f);
+
+            dataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+            data.setValueTextSize(10f);
+            binding.mBarChart.animateXY(1400, 1400);
+            data.setValueTextColor(Color.DKGRAY);
+
+            data.setDrawValues(false);  // removing x values
+            binding.mBarChart.setDrawSliceText(false); // removing y values
+            binding.mBarChart.invalidate();
+        } catch (Exception e) {
+            Toast.makeText(getActivity(), e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }*/
+
     private void getPaymentHistory() {
         showProgressing(getActivity());
         databaseReference.child(Constants.REF_PAYMENTS).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 hideProgressing();
+                paymentList.clear();
                 for (DataSnapshot s: snapshot.getChildren()) {
                     PaymentModel users = s.getValue(PaymentModel.class);
                     if(loggedStore.getId().equalsIgnoreCase(users.getStoreId())){
+                        paymentList.add(users);
                         for(int i=0;i<users.getSaleModel().getProductsList().size();i++){
                             totalMoneyEarned=totalMoneyEarned+Integer.parseInt(users.getSaleModel().getProductsList().get(i).getSalePrice());
                         }
@@ -116,5 +252,18 @@ public class StoreHomeFrg extends BaseFrg {
             }
         });
 
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                //initiatePieChart(paymentList);
+                initiateBarChart2(paymentList);
+            }
+        }, 1000);
+
+
+
+
     }
+
 }
